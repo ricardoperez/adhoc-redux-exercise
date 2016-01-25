75569e99b424dd479b41310733fa8bdcb5712e6a
Tim Ruffles <timruffles@gmail.com>
2016-01-25 19:32:16 +0000

    initial

---
+++ actions/counter.js
@@ -0,0 +1,26 @@
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
+export function increment() {
+  return {
+    type: INCREMENT_COUNTER
+  }
+}
+
+export function decrement() {
+  return {
+    type: DECREMENT_COUNTER
+  }
+}
+
+export function incrementIfOdd() {
+  return (dispatch, getState) => {
+    // TODO implement the logic for incrementIfOdd
+  }
+}
+
+export function incrementAsync(delay = 1000) {
+  return dispatch => {
+    // TODO implement increment async
+  }
+}
---
+++ reducers/counter.js
@@ -0,0 +1,9 @@
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../actions/counter'
+
+export default function counter(state = 0, action) {
+  // TODO implement INCREMENT_COUNTER and DECREMENT_COUNTER
+  switch (action.type) {
+    default:
+      return state
+  }
+}
